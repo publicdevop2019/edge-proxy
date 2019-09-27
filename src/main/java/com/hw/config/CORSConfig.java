a9bae34f20e7558895e494bdf0c6c22a3b19be77
publicdevop2019 <publicdevop2019hw@gmail.com>
2019-09-26 21:43:40 -0400

    update cors config

--- src/main/java/com/hw/config/CORSConfig.java
+++ src/main/java/com/hw/config/CORSConfig.java
@@ -19,6 +19,8 @@ public class CORSConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("*");
         configuration.addAllowedHeader("Authorization");
+        configuration.addAllowedHeader("location");
+        configuration.addAllowedHeader("lastupdateat");
         configuration.addAllowedHeader("Content-Type");
         configuration.addAllowedHeader("Accept");
         configuration.addAllowedHeader("Access-Control-Request-Method");
