db4f1792197e64d5751318a42a15c33ddd4aa666
publicdevop2019 <publicdevop2019hw@gmail.com>
2020-11-20 00:38:02 -0500

    update shared, enable cache, pass test

--- src/main/java/com/hw/aggregate/endpoint/model/BizEndpointQueryRegistry.java
+++ src/main/java/com/hw/aggregate/endpoint/model/BizEndpointQueryRegistry.java
@@ -3,11 +3,21 @@ package com.hw.aggregate.endpoint.model;
 import com.hw.shared.sql.RestfulQueryRegistry;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class BizEndpointQueryRegistry extends RestfulQueryRegistry<BizEndpoint> {
 
     @Override
     public Class<BizEndpoint> getEntityClass() {
         return BizEndpoint.class;
+    }
+    @PostConstruct
+    private void setUp() {
+        cacheable.put(RoleEnum.USER, true);
+        cacheable.put(RoleEnum.ADMIN, true);
+        cacheable.put(RoleEnum.APP, true);
+        cacheable.put(RoleEnum.PUBLIC, true);
+        cacheable.put(RoleEnum.ROOT, true);
     }
 }
--- src/main/java/com/hw/aggregate/revoke_token/model/RevokeTokenQueryRegistry.java
+++ src/main/java/com/hw/aggregate/revoke_token/model/RevokeTokenQueryRegistry.java
@@ -3,6 +3,8 @@ package com.hw.aggregate.revoke_token.model;
 import com.hw.shared.sql.RestfulQueryRegistry;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class RevokeTokenQueryRegistry extends RestfulQueryRegistry<RevokeToken> {
 
@@ -10,5 +12,12 @@ public class RevokeTokenQueryRegistry extends RestfulQueryRegistry<RevokeToken> 
     public Class<RevokeToken> getEntityClass() {
         return RevokeToken.class;
     }
-
+    @PostConstruct
+    private void setUp() {
+        cacheable.put(RoleEnum.USER, true);
+        cacheable.put(RoleEnum.ADMIN, true);
+        cacheable.put(RoleEnum.APP, true);
+        cacheable.put(RoleEnum.PUBLIC, true);
+        cacheable.put(RoleEnum.ROOT, true);
+    }
 }
