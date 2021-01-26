407a9e7a748f933ea44cf406d7583e685881c178
publicdevop2019 <publicdevop2019hw@gmail.com>
2021-01-26 00:01:33 -0500

    fix csrf

--- src/main/java/com/mt/edgeproxy/infrastructure/ResourceServerConfig.java
+++ src/main/java/com/mt/edgeproxy/infrastructure/ResourceServerConfig.java
@@ -1,5 +1,6 @@
 package com.mt.edgeproxy.infrastructure;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,22 +8,24 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.CsrfToken;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Configuration
 @EnableWebFluxSecurity
 public class ResourceServerConfig {
-
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
@@ -42,6 +45,16 @@ public class ResourceServerConfig {
                 .oauth2ResourceServer().jwt()
         ;
         return httpSecurity.build();
+    }
+
+    @Bean
+    WebFilter addCsrfToken() {
+        return (exchange, next) -> exchange
+                .<Mono<CsrfToken>>getAttribute(CsrfToken.class.getName())
+                .doOnSuccess(token -> {
+                    log.debug("csrf token {}", token.getToken());
+                }) // do nothing, just subscribe :/
+                .then(next.filter(exchange));
     }
 
     @Bean
